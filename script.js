86e769f34437519fe3ad2ba9fe723d0c1089caf4
Izentropowy <119621854+Izentropowy@users.noreply.github.com>
2023-07-17 20:35:04 +0200

    Change board on click
    When field is clicked, corresponding
    index of board is changed to player's token

--- script.js
+++ script.js
@@ -1,27 +1,67 @@
+const dimension = 3;
+
+const Field = (row, column) => {
+    let isEmpty = true;
+
+    const toggleStatus = () => {
+        isEmpty = false;
+    }
+
+    const getStatus = () => isEmpty;
+
+    return {
+        row,
+        column,
+        getStatus,
+        toggleStatus,
+    }
+}
+
+const Player = (token) => {
+    let isWinner = false;
+    let points = 0;
+
+    const addPoint = () => {
+        points += 1;
+    }
+
+    const toggleStatus = () => {
+        isWinner = true;
+    }
+
+    const getStatus = () => isWinner;
+
+    const getPoints = () => points;
+
+    return {
+        token,
+        addPoint,
+        toggleStatus,
+        getPoints,
+        getStatus,
+    }
+}
 
 const gameboard = (() => {
-    const dimension = 3;
-
     const createEmptyBoard = () => {
         let newBoard = [];
 
         for (let i = 0; i < dimension; i++) {
             newBoard[i] = [];
             for (let j = 0; j < dimension; j++) {
-                newBoard[i].push("");
+                newBoard[i].push(Field(i, j));
             }   
         }
-
         return newBoard;
     }
-
-    const board = createEmptyBoard();
+    
+    let board = createEmptyBoard();
 
     const getBoard = () => board;
 
-    const updateBoard = (oldBoard, move) => {
-        // logic
-        board = updatedBoard;
+    const updateBoard = (oldBoard, row, column, player) => {
+        oldBoard[row][column] = `${player.token}`;
+        board = oldBoard;
         return board;
     }
 
@@ -31,4 +71,52 @@ const gameboard = (() => {
     };
 })();
 
-console.log(gameboard.getBoard());
+const gameController = (() => {
+    const playerX = Player('x');
+    const playerO = Player('o');
+    const board = gameboard.getBoard();
+
+    let activePlayer = playerX;
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === playerX ? playerO : playerX;
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const playRound = (row, column) => {
+        gameboard.updateBoard(board, row, column, activePlayer);
+        switchPlayerTurn();
+        console.log(board);
+    }
+    return {
+        switchPlayerTurn,
+        getActivePlayer,
+        playRound,
+    }
+})();
+
+const displayController = (() => {
+    const fields = document.querySelectorAll('.game-btn');
+
+    const assignData = (fields) => {
+        fields.forEach((field, index) => {
+            const i = Math.floor(index / dimension);
+            const j = index % dimension;
+            field.setAttribute('data-row', `${i}`);
+            field.setAttribute('data-column', `${j}`);
+        });
+    };
+    
+    assignData(fields);
+
+    fields.forEach(field => field.addEventListener('click', () => 
+    {
+        let row = field.dataset.row;
+        let column = field.dataset.column;
+        gameController.playRound(row, column);
+    }
+        ));
+})();
+
+
